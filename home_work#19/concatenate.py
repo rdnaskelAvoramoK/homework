59998806425a6e63b48b8d43b1e893f6efce2d2c
rdnaskelAvoramoK <komarov.aleksandr.kh@gmail.com>
2019-11-01 01:57:20 +0200

    dell homework19

--- home_work#19/concatenate.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-filename = sys.argv[1]
-file_handler = open(filename,'r')
-
-for line in file_handler:
-    print(line)
-
-file_handler.close()
--- home_work#19/concatenate2.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-for filename in sys.argv:
-    file_handler = open(filename,'r')
-    for line in file_handler:
-        print(line)
-
-    file_handler.close()
