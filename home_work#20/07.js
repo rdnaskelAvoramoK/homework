7cb9d38292e18ab470c387ef98dae2959f2f7972
rdnaskelAvoramoK <komarov.aleksandr.kh@gmail.com>
2019-11-05 16:30:02 +0200

    my firstJS=)

---
+++ home_work#20/07.js
@@ -0,0 +1,6 @@
+/*
+07. Напишите программу, которая в консоли выводит текстовое поздравление. Программа поздравляет того, чье имя определяется в переменной username:
+Happy birthday dear {{username}}
+*/
+let username = 'William Byron'
+console.log('Happy birthday dear ' + username)
