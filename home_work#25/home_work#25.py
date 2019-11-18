dc671882587e65a2163b4e6397055da758be8d2d
rdnaskelAvoramoK <komarov.aleksandr.kh@gmail.com>
2019-11-19 00:20:53 +0200

    home_work#25

---
+++ home_work#25/home_work#25.py
@@ -0,0 +1,25 @@
+class Temperature:
+    def celsius_kelvin(self, gradus):
+        return gradus + 273.15
+
+    def kelvin_celsius(self, gradus):
+        return gradus-273.15
+
+    def celsius_fahrenheit(self, gradus):
+        return (gradus*9/5)+32
+
+    def fahrenheit_celsius(self, gradus):
+        return (gradus-32)*5/9
+
+    def fahrenheit_kelvin(self, gradus):
+        return self.celsius_kelvin(self.fahrenheit_celsius(gradus))
+
+    def kelvin_fahrenheit(self, gradus):
+        return self.celsius_fahrenheit(self.kelvin_celsius(gradus))
+
+
+testTemp1 = Temperature()
+print(testTemp1.kelvin_fahrenheit(100))
+testTemp2 = Temperature()
+print(testTemp2.fahrenheit_kelvin(800))
+print((testTemp1.kelvin_celsius(273.15)))
