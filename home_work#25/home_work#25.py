03cdb0ee6b08af8df0324d4bdfeeb47c46328a33
rdnaskelAvoramoK <komarov.aleksandr.kh@gmail.com>
2019-11-20 15:57:55 +0200

    added spaces and calculation

--- home_work#25/home_work#25.py
+++ home_work#25/home_work#25.py
@@ -3,13 +3,13 @@ class Temperature:
         return gradus + 273.15
 
     def kelvin_celsius(self, gradus):
-        return gradus-273.15
+        return gradus - 273.15
 
     def celsius_fahrenheit(self, gradus):
-        return (gradus*9/5)+32
+        return (gradus * 9 / 5) + 32
 
     def fahrenheit_celsius(self, gradus):
-        return (gradus-32)*5/9
+        return (gradus - 32) * 5/9
 
     def fahrenheit_kelvin(self, gradus):
         return self.celsius_kelvin(self.fahrenheit_celsius(gradus))
@@ -17,9 +17,27 @@ class Temperature:
     def kelvin_fahrenheit(self, gradus):
         return self.celsius_fahrenheit(self.kelvin_celsius(gradus))
 
+    def calculation(self, inputData):
+        data = inputData.split()
+        rezult = []
+        for el in data:
+            if el[-2:].upper() == "*K":
+                rezult.append(round(self.kelvin_celsius(int(el[:-2])), 2))
+            elif el[-2:].upper() == "*F":
+                rezult.append(round(self.fahrenheit_celsius(int(el[:-2])), 2))
+            elif el[-2:].upper() == "*C":
+                rezult.append(round(int(el[:-2]), 2))
+        MaxT = data[(rezult.index(max(rezult)))]
+        MinT = data[(rezult.index(min(rezult)))]
+        average = round(sum(rezult)/len(rezult), 1)
+        print('average temperature is', average)
+        return [data, rezult, (max(rezult), min(rezult)), (MaxT, MinT), average]
+
+
 
 testTemp1 = Temperature()
 print(testTemp1.kelvin_fahrenheit(100))
 testTemp2 = Temperature()
 print(testTemp2.fahrenheit_kelvin(800))
 print(testTemp1.kelvin_celsius(273.15))
+print(testTemp1.calculation("18*C 87*F 300*K 65*C 255*F"))
